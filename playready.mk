ce5809c19733a34e128e373d42e47fa3b0c66053
huangjc <huangjc@rock-chips.com>
2018-07-13 11:54:14 +0800

    init for playready  update PlayReady plugin:     1. read certification from vendor storage and need to write certification using KeyWriter.exe V1.52 first.     2. pass 3229/3328 GTS test     3. based on DPK4.0 source code commit:0b2f0e2e1460010a6e46ebfb83c4dd21ba53f7df

---
+++ playready.mk
@@ -0,0 +1,6 @@
+########################################################
+#add drm function
+#######################################################
+PRODUCT_COPY_FILES += \
+    vendor/widevine/lib/mediadrm/libplayreadydrmplugin.so:$(TARGET_COPY_OUT_VENDOR)/lib/mediadrm/libplayreadydrmplugin
+
